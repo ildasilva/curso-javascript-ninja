58306e18e7be98e0ba7068de4f230c12930d47e1
Ilda Silva <ildaemanoely@hotmail.com>
2019-07-22 21:31:49 -0300

    Update challenge-14.js
    Atualização challenge 14

--- challenge-14/challenge-14.js
+++ challenge-14/challenge-14.js
@@ -11,34 +11,30 @@
   Mostre esse array no console.
   */
   console.log( 'Number Objects Array:' );
-  var numberObjects = [
-    {number: 1},
-    {number: 2},
-    {number: 3},
-    {number: 4},
-    {number: 5},
-    {number: 6},
-    {number: 7},
-    {number: 8},
-    {number: 9},
-    {number: 10}
-  ];
+  var numberObjects = [];
+  for(var cont = 1; cont <= 10; cont ++){
+    return numberObjects.push({number: cont});
+  }
   console.log(numberObjects);
   /*
   Crie um array chamado `justNumbers`, que terá como elementos somente os
   números do array criado acima. Mostre esse novo array no console.
   */
   console.log( '\nJust Numbers:' );
-  // ?
-
+  var justNumbers = numberObjects.map(function(item){
+    return item.number;
+  })
+  console.log(justNumbers);
   /*
   Crie um novo array chamado `justMod2Or3`, que receberá do array criado acima
   somente os números que forem divisíveis por 2 ou 3. Mostre esse novo array
   no console.
   */
   console.log( '\nJust module of division by 2 or 3:' );
-  // ?
-
+  var justMod2Or3 = justNumbers.filter(function(item){
+    return item % 2 === 0 || item % 3 === 0; 
+  });
+  console.log(justMod2Or3);
   /*
   Declare uma variável chamada operation que receba, do array criado acima,
   um valor reduzido pela seguinte operação:
@@ -48,15 +44,20 @@
   Mostre o resultado no console.
   */
   console.log( '\nOperation:' );
-  // ?
-
+  var operation = justMod2Or3.reduce(function(acumulador, atual){
+    return (acumulador + 1) * atual;
+  },0)
+  console.log(operation);
   /*
   Faça o mesmo cálculo passado acima, mas começando do último item para o
   primeiro. O nome da variável deve ser operation2. Mostre o resultado no
   console.
   */
   console.log( '\nOperation 2:' );
-  // ?
+  var operation2 = justMod2Or3.reduceRight(function(acumulador, atual){
+    return (acumulador + 1) * atual;
+  },0)
+  console.log(operation2);
 
   /*
   Crie um array chamado `name`. Cada elemento desse array deve ser uma sílaba
@@ -67,7 +68,9 @@
   falada, como se você estivesse falando em código xD
   */
   console.log( '\nSeu nome na língua do "P":' );
-  // ?
+  var name = ['Il', 'da'].reduce(function(acumulador, atual){
+    return acumulador + 'P' + atual;
+  },'');
 
   /*
   Crie uma variável chamada `inversedName`, que reduzirá o array em uma string
